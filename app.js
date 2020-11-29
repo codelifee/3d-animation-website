379bb82db2fb2bd0705dcd3c4790a886e79c5cf9
codelifee <suminjung09@gmail.com>
2020-11-29 15:40:23 +0900

    Comleted 3d animation

---
+++ app.js
@@ -0,0 +1,44 @@
+//Movement Animation to happen
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+//Items
+const title = document.querySelector('.title');
+const sneaker = document.querySelector('.sneaker img');
+const purchase = document.querySelector('.purchase button');
+const description = document.querySelector('.info h3');
+const sizes = document.querySelector('.sizes');
+
+// Moving Animation Event 
+container.addEventListener('mousemove', (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) /25;
+    let yAxis = (window.innerHeight / 2 - e.pageY) /25 ;
+    console.log(xAxis);
+    console.log(yAxis);
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    // card.style.transform = `rotateY(${0}deg) rotateX(${30}deg)`;
+});
+
+//Animate In  
+container.addEventListener('mouseenter', e => {
+    card.style.transition = 'none';
+    //Popout
+    title.style.transform = 'translateZ(150px)';
+    sneaker.style.transform = 'translateZ(200px) rotateZ(-45deg)';
+    description.style.transform = 'translateZ(150px)';
+    sizes.style.transform = 'translateZ(150px)';
+    purchase.style.transform = 'translateZ(150px)';
+});
+
+//Animate Out
+container.addEventListener('mouseleave', e => {
+    card.style.transform = `rotateY(0deg) rotate(0deg)`;
+    card.style.transition = "all 0.5s ease";
+    //Popback
+    title.style.transform = 'translateZ(0px)';
+    sneaker.style.transform = 'translateZ(0px) rotateZ(0deg)';
+    description.style.transform = 'translateZ(0px)';
+    sizes.style.transform = 'translateZ(0px)';
+    purchase.style.transform = 'translateZ(0px)';
+
+});
